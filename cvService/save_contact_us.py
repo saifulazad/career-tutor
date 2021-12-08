cd52fae8fa3eed4511fe8ce5766ab2573975028c
Muazzeem <muazzem2468@gmail.com>
2021-12-08 09:53:50 +0600

    add google recaptcha

--- cvService/save_contact_us.py
+++ cvService/save_contact_us.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import uuid
+import requests
 
 
 def lambda_handler(event, context):
@@ -11,16 +12,23 @@ def lambda_handler(event, context):
     email = data["email"]
     name = data["name"]
     comment = data["comment"]
-    response = table.put_item(Item={
-        "id": str(uuid.uuid4()),
-        "email": email, "name": name,
-        "comment": comment, "phone": data["phone"] if "phone" in data else []
-    })
+    url = "https://www.google.com/recaptcha/api/siteverify"
+    params = {
+        "secret": "6LeBJ30dAAAAANm4aX9wRTzSnbk-9d7iBMsJkwi9",
+        "response": data["token"],
+    }
+    verify_rs = requests.post(url, data=params)
+    recaptcha_rs = json.loads(verify_rs.text)
+    if recaptcha_rs["success"] is True:
+        table.put_item(Item={
+            "id": str(uuid.uuid4()),
+            "email": email, "name": name,
+            "comment": comment, "phone": data["phone"] if "phone" in data else ""
+        })
     return {
         'headers': {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "*"
         },
         'statusCode': 200,
-        'body': json.dumps(response)
     }
